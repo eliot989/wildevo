70f2e661b6d6c768e5de3dd76611fc6edbe41034
eliot989 <hemegsodon1467@gmail.com>
2026-07-02 15:23:05 +0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,1 +1,26 @@
+let progress = 0;
 
+const fill = document.getElementById("fill");
+const text = document.getElementById("text");
+
+let load = setInterval(() => {
+
+    progress++;
+
+    fill.style.width = progress + "%";
+
+    if(progress < 100){
+        text.innerHTML = progress + "%";
+    }
+
+    if(progress === 100){
+        clearInterval(load);
+
+        text.innerHTML = "PLAY";
+
+        document.getElementById("loadingBar").onclick = () => {
+            window.location.href = "lobby.html";
+        };
+    }
+
+}, 25);
