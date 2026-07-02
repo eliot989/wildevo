9f4ebb4bda13e3745b41f9e3498372e814d7c29a
eliot989 <hemegsodon1467@gmail.com>
2026-07-02 13:03:16 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,11 +2,9 @@ let progress = 0;
 
 const fill = document.getElementById("fill");
 const percent = document.getElementById("percent");
-const loadingBar = document.getElementById("loadingBar");
-const loadingBg = document.getElementById("loadingBg");
-const lobby = document.getElementById("lobby");
 
-window.onload = function () {
+const loadingScreen = document.getElementById("loadingScreen");
+const lobby = document.getElementById("lobby");
 
 let timer = setInterval(() => {
 
@@ -15,27 +13,20 @@ progress++;
 fill.style.width = progress + "%";
 percent.innerText = progress + "%";
 
-if (progress >= 100) {
+if(progress >= 100){
 
 clearInterval(timer);
 
-// PLAY text
 percent.innerText = "PLAY";
 
-// 🔥 IMPORTANT: AUTO SWITCH (fără click)
+// click PLAY -> lobby
 setTimeout(() => {
 
-// ascunde loading complet
-loadingBar.style.display = "none";
-loadingBg.style.display = "none";
-
-// arată lobby
+loadingScreen.style.display = "none";
 lobby.style.display = "block";
 
-}, 300);
+}, 500);
 
 }
 
-}, 25);
-
-};
+}, 30);
