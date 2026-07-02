4d057a335ed0102098a3e1d10246532fc94b5b4e
eliot989 <hemegsodon1467@gmail.com>
2026-07-02 10:38:20 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 let progress = 0;
-let xp = 0;
-let level = 0;
 
-/* LOADING START */
 const fill = document.getElementById("fill");
 const percent = document.getElementById("percent");
+const playBtn = document.getElementById("playBtn");
 
 let loading = setInterval(() => {
+
 progress++;
 
 fill.style.width = progress + "%";
@@ -14,57 +13,14 @@ percent.innerText = progress + "%";
 
 if (progress >= 100) {
 clearInterval(loading);
-showLobby();
+percent.innerText = "READY";
+playBtn.classList.remove("hidden");
 }
 
-}, 25);
+}, 30);
 
-/* SHOW LOBBY */
-function showLobby() {
-document.getElementById("loading").style.display = "none";
+/* ENTER LOBBY */
+function enterLobby() {
+document.getElementById("loadingScreen").style.display = "none";
 document.getElementById("lobby").classList.remove("hidden");
-}
-
-/* PLAY BUTTON (RESET → LOADING AGAIN) */
-function enterGame() {
-document.getElementById("lobby").classList.add("hidden");
-document.getElementById("loading").style.display = "flex";
-
-progress = 0;
-
-loading = setInterval(() => {
-progress++;
-
-fill.style.width = progress + "%";
-percent.innerText = progress + "%";
-
-if (progress >= 100) {
-clearInterval(loading);
-showLobby();
-}
-
-}, 25);
-}
-
-/* XP SYSTEM */
-function addXP(amount) {
-xp += amount;
-
-if (xp >= 100) {
-level++;
-xp = 0;
-document.getElementById("level").innerText = level;
-}
-
-document.getElementById("xpFill").style.width = xp + "%";
-document.getElementById("xpText").innerText = xp + "/100";
-}
-
-/* TASKS */
-function openTasks() {
-document.getElementById("tasks").classList.remove("hidden");
-}
-
-function closeTasks() {
-document.getElementById("tasks").classList.add("hidden");
 }
