126ab7cc18d8cab6a810a28066205dcc989dcfb4
eliot989 <hemegsodon1467@gmail.com>
2026-07-02 13:40:32 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
+let progress = 0;
 
+const fill = document.getElementById("fill");
+const percent = document.getElementById("percent");
+
+const loading = setInterval(function(){
+
+    progress++;
+
+    fill.style.width = progress + "%";
+    percent.innerHTML = progress + "%";
+
+    if(progress >= 100){
+
+        clearInterval(loading);
+
+        percent.innerHTML = "PLAY";
+
+        document.getElementById("loadingBar").style.cursor = "pointer";
+
+        document.getElementById("loadingBar").onclick = function(){
+
+            alert("Lobby-ul va fi adăugat în pasul următor!");
+
+        };
+
+    }
+
+},30);
